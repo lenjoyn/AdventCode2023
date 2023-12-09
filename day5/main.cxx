69bd1e8bd8461aa96f196d00b9b37ae35c553763
lenjoyn <lenjoyn@hotmail.com>
2023-12-09 13:01:41 +0000

    day5 complete

--- day5/main.cxx
+++ day5/main.cxx
@@ -27,7 +27,7 @@ void part1()
     stringstream ss(myline);
 
     ss.ignore(myline.length(),':');
-
+    int seed_num;
     while(ss>>seed_num)
     {
             seeds.push_back(seed_num);
@@ -75,88 +75,6 @@ void part2()
 
     std::map<unsigned, unsigned> ranges;
     std::map<unsigned, bool>     matched;
-    std::getline(myfile, myline);
-    stringstream ss(myline);
-
-    ss.ignore(myline.length(),':');
-
-    unsigned seed_num;
-    unsigned range;
-    while(ss>>seed_num>>range)
-    {
-        ranges[seed_num] = range;
-        matched[seed_num] = false;
-    }
-
-    getline(myfile, myline);
-    unsigned dst, src, num;
-    while(getline(myfile, myline))
-    {
-        if (myline[myline.size()-1] == ':')
-        {
-            std::fill(matched.begin(), matched.end(), false);
-        }
-
-        if (myline == "" || myline[myline.size()-1] == ':')
-            continue;
-
-        stringstream sss(myline);
-        sss>>dst>>src>>num;
-
-        for (auto range: ranges)
-        {
-            if (matched[range.first] == true)
-                continue;
-
-            signed diff = range.first - src;
-            if (abs(diff) > num )
-                continue;
-
-            if (diff < 0)
-            {
-                diff = abs(diff);
-                if (range.first + range.second <= src + num)
-                {
-                    ranges[dst]         = range.second - abs(diff);
-                    ranges[range.first] = diff;
-                    matched[dst]        = true;
-                }
-                else
-                {
-                    ranges[range.first+diff+num] = range.second - diff - num;
-                    ranges[dst]                  = num;
-                    ranges[range.first]          = diff;
-                }
-            }
-            else if (diff >= 0)
-            {
-                int new_range = num - diff;
-                ranges[ranges.first+new_range] = ranges[ranges.first] - new_range;
-                ranges[dst] = new_range;
-                matched[dst] = true;
-            }
-
-        }
-    }
-
-    unsigned lowest_location = UINT_MAX;
-    for (auto seed:seeds)
-    {
-        if (seed > lowest_location)
-            continue;
-        lowest_location = seed;
-    }
-    std::cout<<lowest_location<<std::endl;
-
-}
-
-void part2()
-{
-    std::ifstream myfile("test.txt");
-    std::string myline;
-
-    std::map<unsigned, unsigned> ranges;
-    std::map<unsigned, bool>     matched;
 
     std::getline(myfile, myline);
     stringstream ss(myline);
@@ -178,12 +96,14 @@ void part2()
         {
             for (auto& entry:matched)
                  entry.second = false;
+            
+          //  std::cout<<"xxxxxxxxxxxxx"<<std::endl;
         }
 
         if (myline == "" || myline[myline.size()-1] == ':')
             continue;
 
-        std::cout<<myline<<std::endl;
+        //std::cout<<myline<<std::endl;
         stringstream sss(myline);
         sss>>dst>>src>>num;
 
@@ -191,30 +111,29 @@ void part2()
         for (auto it = ranges.begin(); it != ranges.end(); ++it)
         {
             auto range = *it;
-            std::cout<<"==="<<range.first<<" "<<range.second<<"==="<<std::endl;
+          //  std::cout<<"==="<<range.first<<" "<<range.second<<"==="<<std::endl;
             if (matched[range.first] == true)
                 continue;
 
             signed diff = range.first - src;
-            unsigned abs_diff = abs(diff);
-            if (abs_diff > num  && abs_diff > range.second)
-                continue;
 
             if (diff < 0)
             {
                 diff = abs(diff);
+                if (diff > range.second)
+                    continue;
                 if (range.first + range.second <= src + num)
                 {
-                    std::cout<<"case 1:"<<dst<<" "<<(range.second-diff)<<std::endl;
-                    std::cout<<"case 1:"<<range.first<<" "<<diff<<std::endl;
+            //        std::cout<<"case 1:"<<dst<<" "<<(range.second-diff)<<std::endl;
+            //        std::cout<<"case 1:"<<range.first<<" "<<diff<<std::endl;
                     ranges[dst]         = range.second - diff;
                     matched[dst]        = true;
                 }
                 else
                 {
-                    std::cout<<"case 2:"<<(range.first+diff+num)<<" "<<(range.second - diff - num)<<std::endl;
-                    std::cout<<"case 2:"<<dst<<" "<<num<<std::endl;
-                    std::cout<<"case 2:"<<dst<<" "<<diff<<std::endl;
+            //        std::cout<<"case 2:"<<(range.first+diff+num)<<" "<<(range.second - diff - num)<<std::endl;
+            //        std::cout<<"case 2:"<<dst<<" "<<num<<std::endl;
+            //        std::cout<<"case 2:"<<range.first<<" "<<diff<<std::endl;
                     ranges[range.first+diff+num] = range.second - diff - num;
                     matched[range.first+diff+num]    = false;
 
@@ -225,25 +144,26 @@ void part2()
             }
             else if (diff >= 0)
             {
-
+                if (diff >= num)
+                    continue;
                 if (range.first + range.second <= src + num)
                 {
-                    std::cout<<"case 3:"<<(dst+diff)<<" "<<range.second<<std::endl;
+            //        std::cout<<"case 3:"<<(dst+diff)<<" "<<range.second<<std::endl;
                     ranges[dst+diff] = range.second;
                 }
                 else
                 {
                     int new_range = num - diff;
-                    std::cout<<"case 4:"<<range.first+new_range<<" "<<range.second - new_range<<std::endl;
-                    std::cout<<"case 4:"<<dst+diff<<" "<<new_range<<std::endl;
+            //        std::cout<<"case 4:"<<range.first+new_range<<" "<<range.second - new_range<<std::endl;
+            //        std::cout<<"case 4:"<<dst+diff<<" "<<new_range<<std::endl;
                     ranges[range.first+new_range] = range.second - new_range;
                     ranges[dst+diff] = new_range;
                 }
                 erase_entries.push_back(range.first);
-                ++it;
+//                ++it;
                 matched[dst+diff] = true;
-                if (it == ranges.end())
-                break;
+//                if (it == ranges.end())
+//                break;
             }
             std::cout<<"size: "<<ranges.size()<<std::endl;
         }
@@ -254,13 +174,10 @@ void part2()
         }
     }
 
-    unsigned lowest_location = UINT_MAX;
-    std::cout<<" ==== "<<std::endl;
-    for (auto range:ranges)
-    {
-        std::cout<<range.first<<" "<<range.second<<std::endl;
-    }
-    std::cout<<lowest_location<<std::endl;
+
+    for (auto entry: ranges)
+         std::cout<<entry.first<<" "<<entry.second<<std::endl;
+    std::cout<<(ranges.begin()->first)<<std::endl;
 
 }
 
